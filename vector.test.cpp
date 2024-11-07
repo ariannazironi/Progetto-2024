da2ec0400eb089fa3446169c899e89292d40033e
Arianna Zironi <arianna.zironi03@gmail.com>
2024-11-07 16:00:50 +0100

    Added tests

--- vector.test.cpp
+++ vector.test.cpp
@@ -12,4 +12,20 @@ TEST_CASE("Testing operator +") {
     CHECK(sum.get_x() == 2);
     CHECK(sum.get_y() == 2);
   };
+
+  SUBCASE("Negative components") {
+    const sim::Vector v1{-2, -1};
+    const sim::Vector v2{-3, -2};
+    const sim::Vector sum{v1 + v2};
+    CHECK(sum.get_x() == -5);
+    CHECK(sum.get_y() == -3);
+  };
+
+  SUBCASE("Null components") {
+    const sim::Vector v1{3, 0};
+    const sim::Vector v2{0, 5};
+    const sim::Vector sum{v1 + v2};
+    CHECK(sum.get_x() == 3);
+    CHECK(sum.get_y() == 5);
+  }
 }
