3726fd4d6d1b3f7ab75a5859629b6a56768b0a52
Arianna Zironi <arianna.zironi03@gmail.com>
2024-11-11 11:31:56 +0100

    Merge

--- boid.cpp
+++ boid.cpp
@@ -1,4 +1,5 @@
 #include "boid.hpp"
+#include <vector>
 
 namespace pr {
 
@@ -7,4 +8,13 @@ Boid::Boid(sim::Vector position, sim::Vector velocity)
 
 sim::Vector Boid::get_pos() const { return position_; };
 sim::Vector Boid::get_vel() const { return velocity_; };
+
+//Regola di separazione
+sim::Vector Boid::separation(const std::vector<Boid>& boids){
+
+    for(int i = 0; i < boids.size(); ++i ) {
+        //calcolo distanza da boid corrente a ciascuno
+    }
+};
+
 }  // namespace pr
--- boid.hpp
+++ boid.hpp
@@ -3,16 +3,22 @@
 
 #include "vector.hpp"
 
-namespace pr{class Boid {
-    private:
-    sim::Vector position_;
-    sim::Vector velocity_;
+namespace pr {
+class Boid {
+ private:
+  sim::Vector position_;
+  sim::Vector velocity_;
 
-    public:
-    Boid(sim::Vector position, sim::Vector velocity);
-    sim::Vector get_pos() const;
-    sim::Vector get_vel() const;
+ public:
+  Boid(sim::Vector position, sim::Vector velocity);
+  sim::Vector get_pos() const;
+  sim::Vector get_vel() const;
+
+  // Metodi per le regole di volo
+  sim::Vector separation(const std::vector<Boid>& boid);
+  sim::Vector alignment();
+  sim::Vector cohesion();
 };
-}
+}  // namespace pr
 
 #endif
