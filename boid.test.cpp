aeb7f933a56e44bfe8f400a96652c3dcc87bc75a
Arianna Zironi <arianna.zironi03@gmail.com>
2024-11-13 20:40:42 +0100

    Risolto test metodo separation

--- boid.test.cpp
+++ boid.test.cpp
@@ -1,4 +1,4 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+// #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include "boid.hpp"
 
@@ -50,7 +50,7 @@ TEST_CASE("Testing Boid class") {
   }
 
   SUBCASE("Testing alignment method") {
-    const float a= 0.5;
+    const float a = 0.5;
     float al0_x =
         a * ((b3.get_vel().get_x()) / near.size() - b0.get_vel().get_x());
 
@@ -59,11 +59,11 @@ TEST_CASE("Testing Boid class") {
 
     float al1_x =
         a * ((b3.get_vel().get_x() + b1.get_vel().get_x()) / near_1.size() -
-               b0.get_vel().get_x());
+             b0.get_vel().get_x());
 
     float al1_y =
         a * ((b3.get_vel().get_y() + b1.get_vel().get_y()) / near_1.size() -
-               b0.get_vel().get_y());
+             b0.get_vel().get_y());
     float al2_x =
         a *
         ((b3.get_vel().get_x() + b1.get_vel().get_x() + b2.get_vel().get_x()) /
@@ -87,57 +87,49 @@ TEST_CASE("Testing Boid class") {
   }
 
   SUBCASE("Testing the separation method") {
-    float s = 1.0f;
-    float ds = 5.0f;
+    auto near = b0.find_near(boids, 5.);
+    auto near_1 = b0.find_near(boids, 6.);
+    auto near_2 = b0.find_near(boids, 9.);
 
-    sim::Vector separation_vector = b0.separation(s, ds, near);
-    sim::Vector expected_vector{0., 0.};
-    for (const auto& boid : near) {
-      sim::Vector diff = boid.get_pos() - b0.get_pos();
-      expected_vector += diff * (-s);
-    };
-    CHECK(separation_vector.get_x() ==
-          doctest::Approx(expected_vector.get_x()));
-    CHECK(separation_vector.get_y() ==
-          doctest::Approx(expected_vector.get_y()));
+    sim::Vector separation_vector = b0.separation(1.0f, 5.0f, near);
 
-    sim::Vector separation_vector_1 = b0.separation(s, ds, near_1);
-    sim::Vector expected_vector_1{0., 0.};
-    for (const auto& boid : near_1) {
-      sim::Vector diff = boid.get_pos() - b0.get_pos();
-      expected_vector_1 += diff * (-s);
-    };
-    CHECK(separation_vector_1.get_x() ==
-          doctest::Approx(expected_vector_1.get_x()));
-    CHECK(separation_vector_1.get_y() ==
-          doctest::Approx(expected_vector_1.get_y()));
+    CHECK(separation_vector.get_x() == doctest::Approx(-2.0f).epsilon(0.1));
+    CHECK(separation_vector.get_y() == doctest::Approx(-2.0f).epsilon(0.1));
 
-  sim::Vector separation_vector_2 = b0.separation(s, ds, near_2);
-  sim::Vector expected_vector_2{0., 0.};
-  for (const auto& boid : near_2) {
-    sim::Vector diff = boid.get_pos() - b0.get_pos();
-    expected_vector_2 += diff * (-s);
+    sim::Vector separation_vector_1 = b0.separation(1.0f, 5.5f, near_1);
+
+    CHECK(separation_vector_1.get_x() == doctest::Approx(-5.0f).epsilon(0.1));
+    CHECK(separation_vector_1.get_y() == doctest::Approx(-6.0f).epsilon(0.1));
+
+    sim::Vector separation_vector_2 = b0.separation(1.0f, 8.5f, near_2);
+
+    CHECK(separation_vector_2.get_x() == doctest::Approx(-6.0f).epsilon(0.1));
+    CHECK(separation_vector_2.get_y() == doctest::Approx(-14.0f).epsilon(0.1));
   }
-  CHECK(separation_vector_2.get_x() ==
-        doctest::Approx(expected_vector_2.get_x()));
-  CHECK(separation_vector_2.get_y() ==
-        doctest::Approx(expected_vector_2.get_y()));
-}
 
-SUBCASE("Testing cohesion method") {
-  const float c = 2.0f; 
-  float coh0_x = c * ((b3.get_pos().get_x() / near.size()) - b0.get_pos().get_x());
-  float coh0_y = c * ((b3.get_pos().get_y()) / near.size() - b0.get_pos().get_y());
-  float coh1_x = c * ((b3.get_pos().get_x() + b1.get_pos().get_x() / near_1.size()) - b0.get_pos().get_x());
-  float coh1_y = c * ((b3.get_pos().get_y() + b1.get_pos().get_y() / near_1.size()) - b0.get_pos().get_y());
-  float coh2_x = c * ((b3.get_pos().get_x() + b1.get_pos().get_x() +b2.get_pos().get_x() / near_2.size()) - b0.get_pos().get_x());
-  float coh2_y = c * ((b3.get_pos().get_y() + b1.get_pos().get_y() +b2.get_pos().get_y()/ near_2.size()) - b0.get_pos().get_y());
- CHECK(b0.cohesion(c, near).get_x() == doctest::Approx(coh0_x));
- CHECK(b0.cohesion(c, near).get_y() == doctest::Approx(coh0_y));
- CHECK(b0.cohesion(c, near_1).get_x() == doctest::Approx(coh1_x));
- CHECK(b0.cohesion(c, near_1).get_y() == doctest::Approx(coh1_y));
- CHECK(b0.cohesion(c, near_2).get_x() == doctest::Approx(coh2_x));
- CHECK(b0.cohesion(c, near_2).get_y() == doctest::Approx(coh2_y));
-}
-
+  SUBCASE("Testing cohesion method") {
+    const float c = 2.0f;
+    float coh0_x =
+        c * ((b3.get_pos().get_x() / near.size()) - b0.get_pos().get_x());
+    float coh0_y =
+        c * ((b3.get_pos().get_y()) / near.size() - b0.get_pos().get_y());
+    float coh1_x =
+        c * ((b3.get_pos().get_x() + b1.get_pos().get_x() / near_1.size()) -
+             b0.get_pos().get_x());
+    float coh1_y =
+        c * ((b3.get_pos().get_y() + b1.get_pos().get_y() / near_1.size()) -
+             b0.get_pos().get_y());
+    float coh2_x = c * ((b3.get_pos().get_x() + b1.get_pos().get_x() +
+                         b2.get_pos().get_x() / near_2.size()) -
+                        b0.get_pos().get_x());
+    float coh2_y = c * ((b3.get_pos().get_y() + b1.get_pos().get_y() +
+                         b2.get_pos().get_y() / near_2.size()) -
+                        b0.get_pos().get_y());
+    CHECK(b0.cohesion(c, near).get_x() == doctest::Approx(coh0_x));
+    CHECK(b0.cohesion(c, near).get_y() == doctest::Approx(coh0_y));
+    CHECK(b0.cohesion(c, near_1).get_x() == doctest::Approx(coh1_x));
+    CHECK(b0.cohesion(c, near_1).get_y() == doctest::Approx(coh1_y));
+    CHECK(b0.cohesion(c, near_2).get_x() == doctest::Approx(coh2_x));
+    CHECK(b0.cohesion(c, near_2).get_y() == doctest::Approx(coh2_y));
+  }
 }
