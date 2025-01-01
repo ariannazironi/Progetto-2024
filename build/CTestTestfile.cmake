3607bd4afccc7b9c6f6fee2bd4ad79777450617b
Arianna Zironi <arianna.zironi03@gmail.com>
2025-01-01 15:02:58 +0100

    input parameters

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/arianna-zironi/progetto24
+# Build directory: /home/arianna-zironi/progetto24/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(flock.t "/home/arianna-zironi/progetto24/build/flock.t")
+set_tests_properties(flock.t PROPERTIES  _BACKTRACE_TRIPLES "/home/arianna-zironi/progetto24/CMakeLists.txt;47;add_test;/home/arianna-zironi/progetto24/CMakeLists.txt;0;")
+add_test(boid.t "/home/arianna-zironi/progetto24/build/boid.t")
+set_tests_properties(boid.t PROPERTIES  _BACKTRACE_TRIPLES "/home/arianna-zironi/progetto24/CMakeLists.txt;48;add_test;/home/arianna-zironi/progetto24/CMakeLists.txt;0;")
+add_test(vector.t "/home/arianna-zironi/progetto24/build/vector.t")
+set_tests_properties(vector.t PROPERTIES  _BACKTRACE_TRIPLES "/home/arianna-zironi/progetto24/CMakeLists.txt;49;add_test;/home/arianna-zironi/progetto24/CMakeLists.txt;0;")
